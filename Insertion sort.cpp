0fa89668b0e306f689fcf97e22911093ea15e615
SACHIN SHUKLA <64725812+Sachinshukla0515@users.noreply.github.com>
2021-02-22 18:18:46 +0530

    Create Insertion sort.cpp

---
+++ Insertion sort.cpp
@@ -0,0 +1,20 @@
+/* code for Insertion sort */
+// Time : O(n^2) worst case  O(n) Average
+// space : O(1)
+
+// code begin 
+
+void insertionSort(int arr[], int n){
+   
+    for(int i=0;i<n;i++){
+     int j=i-1;
+     int key=arr[i];
+     
+     while(j>=0 && arr[j]>key){
+        arr[j+1]=arr[j];
+        j--;
+     }
+     arr[j+1]=key;
+    }
+    
+ // Finished   
